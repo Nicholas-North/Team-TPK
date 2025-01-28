f0b4b1d715f9c99382203ef894716d8893265d64
Bxcrnr <bxcrnr@gmail.com>
2025-01-28 09:58:53 -0500

    multi attack and healing
    Added multi attack and healing, as well as some modifiers for attack rolls and damage

--- Code/Simulation.py
+++ Code/Simulation.py
@@ -96,7 +96,7 @@ def initialize_templates() -> List[Template]:
 
 # Player class definition
 class Player:
-    def __init__(self, name="", player_class="", hit_points=0, armor_class=0, movement_speed=0, level=1, strength_score=10, dexterity_score=10, constitution_score=10, intelligence_score=10, wisdom_score=10, charisma_score=10):
+    def __init__(self, name="", player_class="", hit_points=0, armor_class=0, movement_speed=0, level=1, strength_score=10, dexterity_score=10, constitution_score=10, intelligence_score=10, wisdom_score=10, charisma_score=10, multi_attack=0, can_heal=0, num_heals=0):
         self.name = name
         self.player_class = player_class
         self.hit_points = hit_points
@@ -109,9 +109,13 @@ class Player:
         self.intelligence_score = intelligence_score
         self.wisdom_score = wisdom_score
         self.charisma_score = charisma_score
+        self.multi_attack = multi_attack 
+        self.can_heal = can_heal
+        self.num_heals = num_heals  
         self.melee_attack_dict = {
             "Melee Attack": (1, 6)  # Example melee attack with 1d6 damage
         }
+        
         self.templates = []  # List of templates the player can use
 
     def add_template(self, template: Template):
@@ -120,16 +124,16 @@ class Player:
 # Combat Simulation class definition
 class CombatSimulation:
     def __init__(self):
-        # Initialize templates
+        
         self.templates = initialize_templates()
 
-        # Create players
+        
         self.players = [Player(), Player()]
         
-        # Set up player 1 (example: Fighter)
+        
         self.players[0].name = "Player 1"
         self.players[0].player_class = "Fighter"
-        self.players[0].hit_points = 30
+        self.players[0].hit_points = 100
         self.players[0].armor_class = 15
         self.players[0].movement_speed = 6
         self.players[0].level = 1
@@ -139,11 +143,14 @@ class CombatSimulation:
         self.players[0].intelligence_score = 10
         self.players[0].wisdom_score = 8
         self.players[0].charisma_score = 10
+        self.players[0].multi_attack = 1
+        self.players[0].can_heal = 1
         
-        # Set up player 2 (example: Wizard)
+        
+        
         self.players[1].name = "Player 2"
         self.players[1].player_class = "Wizard"
-        self.players[1].hit_points = 18
+        self.players[1].hit_points = 25
         self.players[1].armor_class = 12
         self.players[1].movement_speed = 5
         self.players[1].level = 1
@@ -153,7 +160,10 @@ class CombatSimulation:
         self.players[1].intelligence_score = 16
         self.players[1].wisdom_score = 12
         self.players[1].charisma_score = 10
-
+        self.players[1].charisma_score = 0
+        self.players[1].can_heal = 1
+        self.players[1].num_heals = 3
+        
         # Give players some templates
         self.players[0].add_template(self.templates[7])  # Dash (BA)
         self.players[0].add_template(self.templates[3])  # Melee Weapon Attack
@@ -166,58 +176,82 @@ class CombatSimulation:
         if attack_type in attacker.melee_attack_dict:
             num_dice, die_size = attacker.melee_attack_dict[attack_type]
             # Roll to hit (d20)
-            attack_roll = self.roll_dice(1, 20)
+            attack_roll = self.roll_dice(1, 20) + 1 + ((attacker.strength_score - 10) / 2) #Prof. and attack bonus
             print(f"{attacker.name} attacks {defender.name} with {attack_type} (Roll: {attack_roll})")
 
             if attack_roll >= defender.armor_class:
-                damage = self.roll_dice(num_dice, die_size)  # Roll damage (1d6)
+                damage = self.roll_dice(num_dice, die_size) + ((attacker.strength_score - 10) / 2) # Roll damage (1d6)
                 defender.hit_points -= damage
                 print(f"{defender.name} takes {damage} damage! HP: {defender.hit_points}")
             else:
                 print("Attack missed!")
+                
+    def resolve_heal(self, player):
+        heal_amount = 10  
+        player.hit_points += heal_amount
+        player.num_heals = player.num_heals - 1
+        print(f"{player.name} heals for {heal_amount} HP! New HP: {player.hit_points}")            
 
     # Function to roll a number of dice
     def roll_dice(self, num_dice, die_size):
         return sum(random.randint(1, die_size) for _ in range(num_dice))
 
-    # Run one round of combat
-    def run_round(self):
-        print("=== COMBAT ROUND ===")
-        
-        # Simulate player 1's actions
-        self.perform_actions(self.players[0], self.players[1])
 
-        # Simulate player 2's actions
-        self.perform_actions(self.players[1], self.players[0])
+    
+    def run_round(self):
+        print("=== COMBAT START ===")
+    
+        # Loop until one player has 0 or less health
+        while all(player.hit_points > 0 for player in self.players):
+            # Simulate player 1's actions
+            self.perform_actions(self.players[0], self.players[1])
+
+            # Check if Player 2 is still alive
+            if self.players[1].hit_points <= 0:
+                print(f"{self.players[1].name} has been defeated!")
+                break
+        
+            
+            self.perform_actions(self.players[1], self.players[0])
+
+            # Check if Player 1 is still alive
+            if self.players[0].hit_points <= 0:
+                print(f"{self.players[0].name} has been defeated!")
+                break
 
     def perform_actions(self, player, opponent):
         print(f"\n{player.name}'s turn:")
+
         
-        # Player 1's movement (Dash bonus action)
-        dash_template = self.templates[7]  # Dash (BA)
+        dash_template = self.templates[7]  
         print(f" chooses bonus action: {dash_template.name}")
         dash_template.print_template()
 
-        # Move into range (simplified for demonstration)
+        # Move into range 
         print(f"{player.name} moves into range of {opponent.name}")
 
-        # Player 1's attack (Melee Weapon Attack)
-        melee_attack_template = self.templates[3]  # Melee Weapon Attack
+       
+        melee_attack_template = self.templates[3]  
         print(f" chooses action: {melee_attack_template.name}")
         melee_attack_template.print_template()
         self.resolve_attack(player, opponent, "Melee Attack")
 
-        # Player 2's movement (Disengage bonus action)
-        disengage_template = self.templates[8]  # Disengage (BA)
-        print(f" chooses bonus action: {disengage_template.name}")
-        disengage_template.print_template()
-        print(f"{player.name} moves away from {opponent.name}")
+        # If multi-attack is enabled, perform an additional attack
+        if player.multi_attack == 1:
+            print(f"{player.name} performs a second attack!")
+            self.resolve_attack(player, opponent, "Melee Attack")
 
-        # Player 2's attack (Ranged Weapon Attack)
-        ranged_attack_template = self.templates[4]  # Ranged Weapon Attack
-        print(f" chooses action: {ranged_attack_template.name}")
-        ranged_attack_template.print_template()
-        self.resolve_attack(player, opponent, "Melee Attack")  # Simplifying for this example
+        
+        if player.can_heal == 1 & player.num_heals > 0:
+            heal_template = self.templates[5]  
+            print(f"{player.name} chooses to heal!")
+            
+            heal_template.print_template()
+            self.resolve_heal(player)
+        
+        
+        if opponent.hit_points <= 0:
+            return  # Exit the turn if the opponent is defeated
 
 # Run the simulation
 simulation = CombatSimulation()
